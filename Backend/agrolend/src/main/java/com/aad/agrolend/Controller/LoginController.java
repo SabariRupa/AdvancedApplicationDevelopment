a4390a1644e87e18a2d46b9fa1d05b5ec2ea0a32
SabariRupa <mohanrajmano1973@gmail.com>
2024-03-21 14:41:34 +0530

    Day 7

---
+++ Backend/agrolend/src/main/java/com/aad/agrolend/Controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.aad.agrolend.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.aad.agrolend.Dto.LoginRequest;
+import com.aad.agrolend.Model.Users;
+import com.aad.agrolend.Service.UserService;
+
+
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/api/login")
+    public ResponseEntity<Users> login(@RequestBody LoginRequest loginRequest) {
+        return userService.login(loginRequest);
+    }
+}
+
